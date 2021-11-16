ba680c4a8b10da5742cd519539e5d6a26b3682ad
Samuel Arseneault <samuel.arseneault@polymtl.ca>
2021-11-16 10:13:23 -0500

    Initial commit

---
+++ game.py
@@ -0,0 +1,19 @@
+"""
+Chapitre 11.3
+
+Fonctions pour simuler un combat.
+"""
+
+
+import random
+
+import utils
+from character import *
+from magician import *
+
+
+def deal_damage(attacker, defender):
+	pass
+
+def run_battle(c1, c2):
+	pass
